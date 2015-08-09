fcb2e2a4e37ff2c15dbc6bbfef2378169188754d
Mihail Slavchev <meslav@hotmail.com>
2015-08-09 19:09:15 +0300

    initial proof of concept

--- lib/devices/android/bootstrap/src/io/appium/android/bootstrap/Bootstrap.java
+++ lib/devices/android/bootstrap/src/io/appium/android/bootstrap/Bootstrap.java
@@ -1,5 +1,7 @@
 package io.appium.android.bootstrap;
 
+import java.io.File;
+
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 import io.appium.android.bootstrap.exceptions.SocketServerException;
 import io.appium.android.bootstrap.handler.Find;
@@ -11,6 +13,16 @@ import io.appium.android.bootstrap.handler.Find;
 public class Bootstrap extends UiAutomatorTestCase {
 
   public void testRunServer() {
+    //
+    com.tns.Platform.init2(new File("/data/local/tmp/nsapp"), false);
+    String src =
+    "(function() {\n" + 
+    " var t = 'appium';\n" +
+    " var m = 'Hello from NativeScript!';\n" +
+    " android.util.Log.d(t, m);\n" +
+    "})()";
+    com.tns.Platform.runScript(src);
+    //
     Find.params = getParams();
     boolean disableAndroidWatchers = Boolean.parseBoolean(getParams().getString("disableAndroidWatchers"));
 
